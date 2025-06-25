8abce8851da50e26f350775d2a8b66d32c94beb8
shubhamkaushik <shubham.kaushik004@gmail.com>
2025-06-26 04:00:07 +0530

    added language controller

---
+++ DBOperationsWithEfCore/Controllers/LanguageController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBOperationsWithEfCore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBOperationsWithEfCore.Controllers
+{
+    [Route("api/language")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public LanguageController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllLanguage()
+        {
+            var result = await _appDbContext.Languages.ToListAsync();
+
+            return Ok(result);
+        }
+
+    }
+}
