39e182825c3787b46b2966ab3ef88d73901021fc
shubhamkaushik <shubham.kaushik004@gmail.com>
2025-06-26 03:47:48 +0530

    added controller

---
+++ DBOperationsWithEfCore/Controllers/CurrencyController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBOperationsWithEfCore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBOperationsWithEfCore.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CurrencyController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        [HttpGet("")]
+        public IActionResult GetAllCurrencies()
+        {
+            var result = _appDbContext.Currencies.ToList();
+            return Ok(result);
+
+        }
+    }
+}
